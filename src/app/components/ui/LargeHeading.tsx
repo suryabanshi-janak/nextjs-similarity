10c6034e1c6d60da17362e6f5d82ea8e4f0ce3b5
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-04-01 23:57:55 +0545

    reusable large heading component

---
+++ src/app/components/ui/LargeHeading.tsx
@@ -0,0 +1,39 @@
+import React, { FC, HTMLAttributes, forwardRef } from 'react';
+import { VariantProps, cva } from 'class-variance-authority';
+import { cn } from '@/app/lib/utils';
+
+const largeHeadingVariants = cva(
+  'text-black dark:text-white text-center lg:text-left font-extrabold leading-tight tracking-tighter',
+  {
+    variants: {
+      size: {
+        default: 'text-4xl md:text-5xl lg:text-6xl',
+        lg: 'text-5xl md:text-6xl lg:text-7xl',
+        sm: 'text-2xl md:text-3xl lg:text-4xl',
+      },
+    },
+    defaultVariants: {
+      size: 'default',
+    },
+  }
+);
+
+interface LargeHeadingProps
+  extends HTMLAttributes<HTMLHeadingElement>,
+    VariantProps<typeof largeHeadingVariants> {}
+
+const LargeHeading = forwardRef<HTMLHeadingElement, LargeHeadingProps>(
+  function LargeHeading({ className, size, children, ...props }, ref) {
+    return (
+      <h1
+        ref={ref}
+        {...props}
+        className={cn(largeHeadingVariants({ size, className }))}
+      >
+        {children}
+      </h1>
+    );
+  }
+);
+
+export default LargeHeading;
