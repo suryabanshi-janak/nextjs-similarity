3aacb012fbb06775b88ea16ef28b60811e850356
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-05-27 20:05:21 +0545

    apikeyoptions

--- src/components/ApiDashboard.tsx
+++ src/components/ApiDashboard.tsx
@@ -9,6 +9,7 @@ import LargeHeading from './ui/LargeHeading';
 import Paragraph from './ui/Paragraph';
 import { Input } from './ui/Input';
 import Table from './Table';
+import ApiKeyOptions from './ApiKeyOptions';
 
 const ApiDashboard = async () => {
   const user = await getServerSession(authOptions);
@@ -42,6 +43,7 @@ const ApiDashboard = async () => {
       <div className='flex flex-col items-center justify-center gap-4 lg:justify-start md:flex-row'>
         <Paragraph>Your API key:</Paragraph>
         <Input className='truncate w-fit' readOnly value={activeApiKey.key} />
+        <ApiKeyOptions apiKeyKey={activeApiKey.key} />
       </div>
 
       <Paragraph className='my-4 text-center md:text-start'>
